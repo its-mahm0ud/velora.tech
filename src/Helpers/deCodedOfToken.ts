a6014c4317b371dcf6a73b58a789588a3528d38b
its-mahm0ud <mahmoud.mohamed.faroukk8@gmail.com>
2025-09-23 17:12:20 +0300

    change in token

--- src/Helpers/deCodedOfToken.ts
+++ src/Helpers/deCodedOfToken.ts
@@ -2,12 +2,12 @@
 import { cookies } from "next/headers";
 import { decode } from "next-auth/jwt";
 export default async function getDecodedToken() {
-    const enCodeToken = (await cookies()).get("next-auth.session-token")?.value
+    const cookiesStore = (await cookies())
+    const enCodeToken = cookiesStore.get("next-auth.session-token")?.value || cookiesStore.get("__Secure-next-auth.session-token")?.value
     const deCodeToken = await decode({
         token: enCodeToken,
         secret: process.env.AUTH_SECRET!
     })
-    console.log("tokennnnnnnn", deCodeToken?.token);
 
     return deCodeToken!.token
 
