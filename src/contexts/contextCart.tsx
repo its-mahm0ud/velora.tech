d868a72f838e83f8d793cb6e11a8192586b7d2d4
its-mahm0ud <mahmoud.mohamed.faroukk8@gmail.com>
2025-10-13 15:21:21 +0300

    Fixed ContactCart and Fixed LoadingIcon Navbar in it

--- src/contexts/contextCart.tsx
+++ src/contexts/contextCart.tsx
@@ -1,101 +1,109 @@
-"use client"
-import { apiServices } from '@/Services/ApisServices';
-import React, { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react'
-
+"use client";
+import { apiServices } from "@/Services/ApisServices";
+import { useSession } from "next-auth/react";
+import React, {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 
 export const cartContext = createContext<{
-    cartId: string,
-    imageProduct: string[],
-    userId: string,
-    wishListtCount: number,
-    cartCount: number,
-    OrdersCountCount: number,
-    setCartCount: Dispatch<SetStateAction<number>>,
-    setWishListtCount: Dispatch<SetStateAction<number>>,
-    loadingCartCount: boolean,
-    loadingWishListCount: boolean,
-    loadingOrdersCount: boolean,
+  cartId: string;
+  imageProduct: string[];
+  userId: string;
+  wishListtCount: number;
+  cartCount: number;
+  OrdersCountCount: number;
+  setCartCount: Dispatch<SetStateAction<number>>;
+  setWishListtCount: Dispatch<SetStateAction<number>>;
+  loadingCartCount: boolean;
+  loadingWishListCount: boolean;
+  loadingOrdersCount: boolean;
 }>({
-    cartCount: 0,
-    OrdersCountCount: 0,
-    wishListtCount: 0,
-    imageProduct: [],
-    cartId: "",
-    userId: "",
-    loadingWishListCount: true,
-    loadingOrdersCount: true,
-    loadingCartCount: true,
-    setCartCount: () => { },
-    setWishListtCount: () => { }
+  cartCount: 0,
+  OrdersCountCount: 0,
+  wishListtCount: 0,
+  imageProduct: [],
+  cartId: "",
+  userId: "",
+  loadingWishListCount: true,
+  loadingOrdersCount: true,
+  loadingCartCount: true,
+  setCartCount: () => {},
+  setWishListtCount: () => {},
 });
 
-export default function ContextCartProvider({ children }: { children: ReactNode }) {
+export default function ContextCartProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = useSession();
 
-    // {Cart Count}
-    const [cartCount, setCartCount] = useState(0);
-    const [loadingCartCount, setLoadingCartCount] = useState(true);
-    // {Wish List Count}
-    const [wishListtCount, setWishListtCount] = useState(0);
-    const [loadingWishListCount, setLoadingWishListCount] = useState(true);
-    // {Orders Count}
-    const [OrdersCountCount, setOrdersCount] = useState(0);
-    const [loadingOrdersCount, setLoadingOrdersCount] = useState(true);
-    // {Cart Id}
-    const [cartId, setCartId] = useState("");
-    // {User Id}
-    const [userId, setUserId] = useState("");
-    // {Display animation Image}
-    const [imageProduct, setImageProduct] = useState<string[]>([]);
+  // {Cart Count}
+  const [cartCount, setCartCount] = useState(0);
+  const [loadingCartCount, setLoadingCartCount] = useState(true);
+  // {Wish List Count}
+  const [wishListtCount, setWishListtCount] = useState(0);
+  const [loadingWishListCount, setLoadingWishListCount] = useState(true);
+  // {Orders Count}
+  const [OrdersCountCount, setOrdersCount] = useState(0);
+  const [loadingOrdersCount, setLoadingOrdersCount] = useState(true);
+  // {Cart Id}
+  const [cartId, setCartId] = useState("");
+  // {User Id}
+  const [userId, setUserId] = useState("");
+  // {Display animation Image}
+  const [imageProduct, setImageProduct] = useState<string[]>([]);
 
-    async function getWishListCount() {
-        setLoadingWishListCount(true)
-        const res = await apiServices.getAllProductToWishList();
-        setWishListtCount(res.count)
-        setLoadingWishListCount(false)
-    }
-    async function getOrdersCount() {
-        setLoadingOrdersCount(true)
-        const res = await apiServices.getUserOrders(userId)
-        setOrdersCount(res.length);
-        setLoadingOrdersCount(false)
-    }
+  async function getWishListCount() {
+    setLoadingWishListCount(true);
+    const res = await apiServices.getAllProductToWishList();
+    setWishListtCount(res.count);
+    setLoadingWishListCount(false);
+  }
+  async function getOrdersCount() {
+    setLoadingOrdersCount(true);
+    const res = await apiServices.getUserOrders(userId);
+    setOrdersCount(res.length);
+    setLoadingOrdersCount(false);
+  }
 
-    async function getAllProductsSp() {
-        const res = await apiServices.fetchProducts()
-        const allProductImage = res.data;
-        const allImage = allProductImage.flatMap(image => image.images);
-        setImageProduct(allImage)
+  async function getAllProductsSp() {
+    const res = await apiServices.fetchProducts();
+    const allProductImage = res.data;
+    const allImage = allProductImage.flatMap((image) => image.images);
+    setImageProduct(allImage);
+  }
 
-    }
+  async function getUserId() {
+    const res = await apiServices.getUserId();
+    setUserId(res.decoded.id);
+  }
+  async function getUser() {
+    setLoadingCartCount(true);
+    const res = await apiServices.displayCartUser();
+    setCartCount(res.numOfCartItems);
+    setCartId(res.cartId);
 
-    async function getUserId() {
-        const res = await apiServices.getUserId();
-        setUserId(res.decoded.id)
-    }
-    async function getUser() {
-        setLoadingCartCount(true)
-        const res = await apiServices.displayCartUser();
-        setCartCount(res.numOfCartItems);
-        setCartId(res.cartId)
+    setLoadingCartCount(false);
+  }
+  useEffect(() => {
+    getUser();
+    getUserId();
+    getAllProductsSp();
+    getWishListCount();
+  }, [session]);
+  useEffect(() => {
+    getOrdersCount();
+  }, [userId,session]);
 
-
-        setLoadingCartCount(false)
-    }
-    useEffect(() => {
-        getUser();
-        getUserId();
-        getAllProductsSp()
-        getWishListCount()
-
-    }, [])
-    useEffect(() => {
-        getOrdersCount()
-    }, [userId])
-
-
-
-
-    return (<cartContext.Provider value={{
+  return (
+    <cartContext.Provider
+      value={{
         cartCount,
         setCartCount,
         loadingCartCount,
@@ -106,10 +114,10 @@ export default function ContextCartProvider({ children }: { children: ReactNode 
         setWishListtCount,
         loadingWishListCount,
         OrdersCountCount,
-        loadingOrdersCount
-    }}>
-        {children}
+        loadingOrdersCount,
+      }}
+    >
+      {children}
     </cartContext.Provider>
-
-    )
+  );
 }
